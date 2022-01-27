6b9dad6b8d66a77067ac13de93fa675b502cff07
men100 <men100per@gmail.com>
2022-01-27 15:11:07 +0900

    My_Code_2_01_3.cpp: Add my code

---
+++ codes/cpp/My_Code_2_01_3.cpp
@@ -0,0 +1,15 @@
+// 問題 2.1.3 (問題 ID : 004)
+#include <iostream>
+#include <string>
+using namespace std;
+
+int A1, A2, A3;
+int ans;
+
+int main() {
+	cin >> A1 >> A2 >> A3;
+	ans = A1 * A2 * A3;
+	cout << ans << endl;
+	
+	return 0;
+}
